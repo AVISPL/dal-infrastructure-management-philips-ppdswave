993c58a79e08e326f4cb69c5e1d74b844a3c0ec1
Maksym.Rossiytsev <maksym.rossiitsev@avispl.com>
2023-07-10 14:57:49 +0300

    SYAL-1339 DAL: Philips Wave

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # dal-infrastructure-management-philips-wave
-DAL Repository dedicated to Phillips PPDS Wave, an evolutionary cloud platform unlocking the power, versatility and intelligence inside Philips professional displays
+DAL Repository dedicated to Phillips PPDS Wave, an evolutionary cloud platform unlocking the power, 
+versatility and intelligence inside Philips professional displays
+
+Currently monitored property categories are:
+- Device Metadata
+- Alerts
+- Audio
+- Bookmarks
+- Customer
+- Firmware
+- NetworkInformation
+- NetworkInterfaces
+- Platform
+- Power
+- PowerSchedule
+- Screenshot
+- Site
+- System
+- Video
+
+Currently supported control operations are:
+- Reboot
+- Audio Mute
+- Volume
+- Brightness
+- Power Mode
+- Orientation
+- Input Source
+- Screenshot
+- IR Control Mode
+- Keyboard Control Mode
+- Ports Control Mode
+- Display Alias
+- Led Strip Color
+
---
+++ src/main/java/com/avispl/symphony/dal/communicator/dto/display/power/PowerSchedule.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2023 AVI-SPL, Inc. All Rights Reserved.
+ */
+package com.avispl.symphony.dal.communicator.dto.display.power;
+
+/**
+ * Power schedule wrapper
+ *
+ * @author Maksym.Rossiytsev
+ * @since 1.0.0
+ * */
+public class PowerSchedule {
+    private boolean isSynced;
+    private LatestJob latestJob;
+    private Schedule schedule;
+
+    /**
+     * Retrieves {@link #isSynced}
+     *
+     * @return value of {@link #isSynced}
+     */
+    public boolean getIsSynced() {
+        return isSynced;
+    }
+
+    /**
+     * Sets {@link #isSynced} value
+     *
+     * @param isSynced new value of {@link #isSynced}
+     */
+    public void setIsSynced(boolean isSynced) {
+        this.isSynced = isSynced;
+    }
+
+    /**
+     * Retrieves {@link #latestJob}
+     *
+     * @return value of {@link #latestJob}
+     */
+    public LatestJob getLatestJob() {
+        return latestJob;
+    }
+
+    /**
+     * Sets {@link #latestJob} value
+     *
+     * @param latestJob new value of {@link #latestJob}
+     */
+    public void setLatestJob(LatestJob latestJob) {
+        this.latestJob = latestJob;
+    }
+
+    /**
+     * Retrieves {@link #schedule}
+     *
+     * @return value of {@link #schedule}
+     */
+    public Schedule getSchedule() {
+        return schedule;
+    }
+
+    /**
+     * Sets {@link #schedule} value
+     *
+     * @param schedule new value of {@link #schedule}
+     */
+    public void setSchedule(Schedule schedule) {
+        this.schedule = schedule;
+    }
+}
